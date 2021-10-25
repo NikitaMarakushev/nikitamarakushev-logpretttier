e0d7c8081a748d97820c360d1a392d885de5bd3b
NikitaMarakushev <nikitamarakushev@yandex.ru>
2021-10-25 19:05:33 +0300

    execution script variables

--- index.php
+++ index.php
@@ -12,6 +12,24 @@ require 'vendor/autoload.php';
 $log = new Logger('main');
 $log->pushHandler(new StreamHandler('logs/error.log', Logger::ERROR));
 
+if ($argv[0] === 'index.php' || count($argv) > 2) {
+    print_r("logpretttier ERROR: invalid syntax of command, for help run, please 'php index.php -h' \n");
+    die;
+}
+
+if ($argv[1] === '-h') {
+    print_r("Usage: php index.php [FILENAME] COMMAND \n\nOptions: \n" .
+        "-h \t Help information about script usage" .
+        "FILENAME \t name of file, you would like to format \n" .
+        "-v \t shows current version of script \n"
+    );
+    die;
+}
+
+if ($argv[1] === '-v') {
+    print_r("logpretttier verstion 0.0.1 \n");
+    die;
+}
 
 try {
     $formatterDirector = new FormatterDirector();
